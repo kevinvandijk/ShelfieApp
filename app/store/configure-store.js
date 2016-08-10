f13f1c9fc8c3cae291c70967ff9cd175597d6456
Kevin van Dijk <kevin@edgybits.com>
2016-08-10 02:53:16 +0200

    Add redux-logger

--- app/store/configure-store.js
+++ app/store/configure-store.js
@@ -1,8 +1,14 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore } from 'redux';
+import createLogger from 'redux-logger';
+
 import reducer from '../reducer';
 
+const logger = createLogger();
 export default function configureStore() {
-  const store = createStore(reducer);
+  const store = createStore(
+    reducer,
+    applyMiddleware(logger) // Logger must be the last middleware in chain
+  );
 
   return store;
 }
