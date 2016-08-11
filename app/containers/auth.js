cf9d6594912831f5401a15600a73aba3cf286d74
Kevin van Dijk <kevin@edgybits.com>
2016-08-11 02:45:01 +0200

    Add and link react-native-vector-icons

--- app/containers/auth.js
+++ app/containers/auth.js
@@ -4,7 +4,7 @@ import { View, Text } from 'react-native';
 export default class AuthContainer extends Component {
   render() {
     return (
-      <View>
+      <View style={{top: 100, flex: 1}}>
         <Text>Auth</Text>
       </View>
     );
