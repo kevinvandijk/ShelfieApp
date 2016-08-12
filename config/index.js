7619ddd1f5d611136ed558528a3203b15d163ea8
Kevin van Dijk <kevin@edgybits.com>
2016-08-12 03:38:59 +0200

    Make config fall back with nested objects as well

--- config/index.js
+++ config/index.js
@@ -9,12 +9,17 @@ const composedConfig = Immutable.from({
   ...localConfig
 });
 
+// TODO: memoize results
 class Config {
   get(path) {
-    const value = get(composedConfig, path);
+    let value = get(composedConfig, path);
+    // Try one more time in default config
+    // TODO: Make deep merge to not have to do 2 lookups
     if (isUndefined(value)) {
-      throw new Error(`No config value found for '${path}'`);
+      value = get(defaultConfig, path);
+      if (isUndefined(value)) throw new Error(`No config value found for '${path}'`);
     }
+
     return value;
   }
 }
