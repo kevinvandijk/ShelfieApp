7502995726cdcde1c301d72ea0b96f29b8a4d7ba
Kevin van Dijk <kevin@edgybits.com>
2016-08-12 03:24:14 +0200

    Add ability to enable or disable storage with config file

--- app/store/configure-store.js
+++ app/store/configure-store.js
@@ -1,3 +1,4 @@
+import config from '../../config';
 import { applyMiddleware, createStore } from 'redux';
 import {
   reducer as createStorageReducer,
@@ -10,30 +11,45 @@ import createSagaMiddleware from 'redux-saga';
 import createLogger from 'redux-logger';
 import Reactotron from 'reactotron';
 
-import reducer from '../reducer';
+import combinedReducer from '../reducer';
 import sagas from '../sagas';
 
-const storageEngine = createStorageEngine('save-key');
-const storageMiddleware = createStorageMiddleware(storageEngine);
+const reducer = (config.get('storage.enabled')
+  ? createStorageReducer(combinedReducer)
+  : combinedReducer
+);
+
+const middlewares = [];
+
+let storageEngine;
+if (config.get('storage.enabled')) {
+  storageEngine = createStorageEngine(config.get('storage.engineKey'));
+  const storageMiddleware = createStorageMiddleware(storageEngine);
+  middlewares.push(storageMiddleware);
+}
+
 const sagaMiddleware = createSagaMiddleware();
+middlewares.push(sagaMiddleware);
+
+// Logger and Reactotron must be the last middleware in chain
 const logger = createLogger();
+middlewares.push(logger);
+
+if (__DEV__) middlewares.push(Reactotron.reduxMiddleware);
 
 export default function configureStore() {
   const store = createStore(
     createStorageReducer(reducer),
-    applyMiddleware(
-      sagaMiddleware,
-      storageMiddleware,
-      logger,
-      Reactotron.reduxMiddleware
-    ) // Logger and Reactotron must be the last middleware in chain
+    applyMiddleware(...middlewares)
   );
 
   if (__DEV__) Reactotron.addReduxStore(store); //eslint-disable-line
 
-  createStorageLoader(storageEngine)(store)
-    .then((state) => console.log('Loaded state:', state)) //eslint-disable-line
-    .catch(() => console.log('Failed to load previous state')); //eslint-disable-line
+  if (config.get('storage.enabled')) {
+    createStorageLoader(storageEngine)(store)
+      .then((state) => console.log('Loaded state:', state)) //eslint-disable-line
+      .catch(() => console.log('Failed to load previous state')); //eslint-disable-line
+  }
 
   sagaMiddleware.run(sagas);
 
