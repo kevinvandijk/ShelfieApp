728475c4773270cac25b83be809ae45504626939
Kevin van Dijk <kevin@edgybits.com>
2016-09-07 01:53:46 +0200

    Add support for keychain access

---
+++ app/services/keychain.js
@@ -0,0 +1,14 @@
+import Keychain from 'react-native-keychain';
+
+// TODO: Make configurable name
+async function getAuthToken() {
+  return Keychain.getInternetCredentials('shelfie-auth-token');
+}
+
+function setAuthToken(token) {
+  return Keychain.setInternetCredentials('shelfie-auth-token', null, token);
+}
+
+export default {
+  setAuthToken
+};
