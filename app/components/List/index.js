88f7fa4aafd55e8e07a78cbf79be2787a8064866
Kevin van Dijk <kevin@edgybits.com>
2016-09-18 02:12:39 +0200

    Add prototype for List with hard coded data

--- app/components/List/index.js
+++ app/components/List/index.js
@@ -1,18 +1,20 @@
 import React, { Component, PropTypes } from 'react';
-import { ListView, Text, View } from 'react-native';
+import { ListView, Text, View, TouchableHighlight } from 'react-native';
 import styles from './styles';
-const { string } = PropTypes;
+const { string, func } = PropTypes;
+import { Actions } from 'react-native-router-flux'
 
 const Row = (props) => {
   return (
-    <View style={ styles.rowContainer }>
+    <TouchableHighlight style={ styles.rowContainer } onPress={ props.onPress }>
       <Text style={ styles.rowText }>{ props.text }</Text>
-    </View>
+    </TouchableHighlight>
   );
 };
 
 Row.propTypes = {
-  text: string.isRequired
+  text: string.isRequired,
+  onPress: func
 };
 
 const SectionHeader = (props) => {
@@ -40,7 +42,32 @@ export default class List extends Component {
     });
 
     const data = {
-
+      1979: [
+        { text: 'Wouter speelt in de tuin' },
+        { text: 'Wouter zwemt in het water' },
+        { text: 'Alexander presenteert het nieuws' },
+        { text: 'Vakantie Portugal' },
+        { text: 'Wouter rookt een sigaretje' },
+        { text: 'Speeldband' }
+      ],
+      1980: [
+        { text: 'Wouter & Kevin spelen samen' },
+        { text: 'Verjaardag Catrien' },
+        { text: 'Kerst bij Oma' },
+        { text: 'Wouter maakt al het speelgoed stuk' },
+        { text: 'Pasen bij tante Anneke' },
+        { text: 'Vakantie Duitsland' },
+        { text: 'Wouter voetballen bij Margriet' },
+        { text: 'Wouter springt op en neer op het gras' },
+        { text: 'Wouter vermaakt zich kostelijk'}
+      ],
+      2016: [
+        { text: 'Wouter begint project KIP' },
+        { text: 'Wouter eet een kippetje' },
+        { text: 'Wouter wordt supergroot en sterk van kip' },
+        { text: 'Wouter start Shelfie' },
+        { text: 'Wouter kijkt deze video ' }
+      ]
     };
 
     this.state = {
@@ -48,8 +75,18 @@ export default class List extends Component {
     };
   }
 
-  renderRow(rowData, sectionId, rowId) {
-    return <Row { ...rowData } key={ `${sectionId}-${rowId}` } />;
+  onRowPress() {
+    Actions['mainWatch']();
+  }
+
+  renderRow = (rowData, sectionId, rowId) => {
+    return (
+      <Row
+        { ...rowData }
+        key={ `${sectionId}-${rowId}` }
+        onPress={ this.onRowPress }
+      />
+    );
   }
 
   renderSectionHeader(sectionData, sectionId) {
