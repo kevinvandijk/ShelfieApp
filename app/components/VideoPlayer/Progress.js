18f5e6f59e2dfc2f5c8fd6dd4b99322ea1e2851b
Kevin van Dijk <kevin@edgybits.com>
2016-09-18 03:39:52 +0200

    Remove animation code and progress bar view in favor of disabled native slider component

--- app/components/VideoPlayer/Progress.js
+++ app/components/VideoPlayer/Progress.js
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react';
-import { View, Animated, Easing, StyleSheet, Text, Slider } from 'react-native';
+import { View, StyleSheet, Text, Slider } from 'react-native';
 import styles from './styles';
 const { number, oneOfType, object, func, array, bool } = PropTypes;
 
@@ -17,8 +17,6 @@ const validateOnSeekProp = (props) => {
 // TODO: Fix proptype for style, it always needs a width!
 class Progress extends React.Component {
   static propTypes = {
-    easing: func,
-    easingDuration: number,
     currentTime: number.isRequired,
     duration: number.isRequired,
     style: oneOfType([number, object, array]).isRequired,
@@ -30,38 +28,8 @@ class Progress extends React.Component {
   }
 
   static defaultProps = {
-    easing: Easing.inOut(Easing.ease),
-    easingDuration: 150,
     currentTime: 0,
     duration: 0
-  }
-
-  constructor(props) {
-    super();
-    this.progressWidth = new Animated.Value(props.currentTime || 0);
-  }
-
-  state = {
-    progress: new Animated.Value(0)
-  }
-
-  componentWillUpdate(nextProps) {
-    if (nextProps.disableSeek) {
-      const style = StyleSheet.flatten(nextProps.style);
-      if (nextProps.currentTime !== this.props.currentTime) {
-        const progress = nextProps.currentTime / nextProps.duration;
-        const width = style.width * progress;
-        this.animate(width);
-      }
-    }
-  }
-
-  animate(width) {
-    Animated.timing(this.progressWidth, {
-      easing: this.props.easing,
-      duration: this.props.easingDuration,
-      toValue: width
-    }).start();
   }
 
   formatSeconds(seconds) {
@@ -76,18 +44,11 @@ class Progress extends React.Component {
 
   render() {
     const { disableTotal, disablePlayed, currentTime, duration, style, disableSeek } = this.props;
-    let minimumTrackTintColor;
-    let maximumTrackTintColor;
 
     const progressBarContainerStyle = styles.progressBarContainer;
     const progressBarStyle = [styles.progressBar, { width: this.progressWidth }];
-
-    if (!disableSeek) {
-      const containerStyle = StyleSheet.flatten(progressBarContainerStyle);
-      const barStyle = StyleSheet.flatten(progressBarStyle);
-      minimumTrackTintColor = barStyle.backgroundColor;
-      maximumTrackTintColor = containerStyle.backgroundColor;
-    }
+    const minimumTrackTintColor = StyleSheet.flatten(progressBarStyle).backgroundColor;
+    const maximumTrackTintColor = StyleSheet.flatten(progressBarContainerStyle).backgroundColor;
 
     return (
       <View style={ style }>
@@ -107,21 +68,16 @@ class Progress extends React.Component {
           </View>
         }
 
-        { disableSeek ?
-          <View style={ progressBarContainerStyle }>
-            <Animated.View style={ progressBarStyle } />
-          </View>
-          :
-          <Slider
-            minimumTrackTintColor={ minimumTrackTintColor }
-            maximumTrackTintColor={ maximumTrackTintColor }
-            thumbImage={ require('../../images/progress-bar-thumb.png') }
-            maximumValue={ duration }
-            value={ currentTime }
-            onValueChange={ this.props.onSeek }
-            onSlidingComplete={ this.props.onSeekComplete }
-          />
-        }
+        <Slider
+          disabled={ disableSeek }
+          minimumTrackTintColor={ minimumTrackTintColor }
+          maximumTrackTintColor={ maximumTrackTintColor }
+          thumbImage={ require('../../images/progress-bar-thumb.png') }
+          maximumValue={ duration }
+          value={ currentTime }
+          onValueChange={ this.props.onSeek }
+          onSlidingComplete={ this.props.onSeekComplete }
+        />
       </View>
     );
   }
