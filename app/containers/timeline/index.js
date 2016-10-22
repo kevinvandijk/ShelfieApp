486365b42f052ff305b5f64fdaf7fb9db2b217e9
Kevin van Dijk <kevin@edgybits.com>
2016-10-22 15:55:13 +0200

    Fetch videos when loading timeline

--- app/containers/timeline/index.js
+++ app/containers/timeline/index.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { View, Text, ListView } from 'react-native';
+import { fetchVideos } from '../../modules/videos';
 import List from '../../components/List'
 
 class TimelineContainer extends Component {
 
+  componentDidMount() {
+    this.props.fetchVideos();
+  }
+
   renderRow(rowData) {
 
   }
+
   render() {
     return (
-      <List
+      <List data={ this.props.videos }
       />
     );
   }
 }
 
-export default TimelineContainer;
+const mapDispatchToProps = {
+  fetchVideos
+};
+
+const mapStateToProps = (state) => {
+  return {
+    videos: state.videos || []
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(TimelineContainer);
