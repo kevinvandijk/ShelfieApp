6e69ea09cbaea9eae00548d98697d3daf1ad1d79
Kevin van Dijk <kevin@edgybits.com>
2016-11-10 02:51:53 +0100

    Activate Dutch translations

--- app/initializers/i18n.js
+++ app/initializers/i18n.js
@@ -3,7 +3,8 @@ import I18n from '../lib/i18n';
 
 export default function i18nInitializer() {
   I18n.setTranslations({
-    en: require('../../config/locales/en.json') // eslint-disable-line
+    en: require('../../config/locales/en.json'),
+    nl: require('../../config/locales/nl.json')
   });
 
   I18n.switchLanguage(getDeviceLocale());
