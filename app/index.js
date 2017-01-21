2f07f07575fe6eff4b1c770051715b938a7aa204
Kevin van Dijk <kevin@edgybits.com>
2017-01-21 22:59:10 +0100

    Add codepush support

--- app/index.js
+++ app/index.js
@@ -1,5 +1,7 @@
 import React, { Component, PropTypes } from 'react';
+import { Alert } from 'react-native';
 import { Provider } from 'react-redux';
+import codePush from 'react-native-code-push';
 import Router from './router';
 
 // Initializers
@@ -7,9 +9,16 @@ import i18nInitializer from './initializers/i18n';
 
 i18nInitializer();
 
+@codePush
 export default class App extends Component {
   static propTypes = {
     store: PropTypes.object.isRequired
+  }
+
+  codePushStatusDidChange(status) {
+    if (status === codePush.SyncStatus.UPDATE_INSTALLED) {
+      Alert.alert('App was updated');
+    }
   }
 
   render() {
