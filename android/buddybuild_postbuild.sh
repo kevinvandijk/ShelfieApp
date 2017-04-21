bcd78fab8550d76e05c7493f2e0feeb4155415fa
Kevin van Dijk <kevin@edgybits.com>
2017-04-21 04:06:05 +0200

    Add sourcemap build for sentry

--- android/buddybuild_postbuild.sh
+++ android/buddybuild_postbuild.sh
@@ -5,4 +5,25 @@ MANIFEST_PATH = "$APP_PATH/android/app/src/main/AndroidManifest.xml"
 
 export VERSION_NUMBER="$(grep versionName $MANIFEST_PATH | sed 's/[^0-9\.]*//g')"
 export BUILD_NUMBER="$(grep versionCode $MANIFEST_PATH | sed 's/[^0-9]*//g')"
+
+CURRENT_DIR=$(pwd)
+
+cd $APP_PATH
+
+mkdir -p build
+cd build
+
+BUNDLE_FILE="main.jsbundle"
+
+node "node_modules/react-native/local-cli/cli.js" bundle \
+  --entry-file index.android.js \
+  --platform android \
+  --dev false \
+  --reset-cache \
+  --bundle-output "$BUNDLE_FILE" \
+  --assets-dest "$BUDDYBUILD_WORKSPACE/app/build" \
+  --sourcemap-output "$BUNDLE_FILE.map"
+
+  cd $CURRENT_DIR
+
 $APP_PATH/scripts/create-sentry-release.sh
--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
 export GIT_REV="$(git rev-parse --short HEAD)"
+
+# For sentry to know where to post errors to
+REVISION_FILE="./config/revision.json"
+echo "{\"revision\": \"${GIT_REV}\"}" > $REVISION_FILE
