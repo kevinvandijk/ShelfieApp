44b21a5dea706ab4e6853ba303793e4b1270e463
Kevin van Dijk <kevin@edgybits.com>
2017-04-21 20:01:27 +0200

    This should do it

--- android/buddybuild_postbuild.sh
+++ android/buddybuild_postbuild.sh
@@ -4,7 +4,7 @@ APP_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 MANIFEST_PATH="$APP_PATH/android/app/src/main/AndroidManifest.xml"
 
 export VERSION_NUMBER="$(grep versionName $MANIFEST_PATH | sed 's/[^0-9\.]*//g')"
-export BUILD_NUMBER="$(grep versionCode $MANIFEST_PATH | sed 's/[^0-9]*//g')"
+export BUILD_NUMBER="$BUDDYBUILD_BUILD_NUMBER"
 
 CURRENT_DIR=$(pwd)
 
