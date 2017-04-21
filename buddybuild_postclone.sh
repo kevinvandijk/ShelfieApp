e9b3c7f5aaf24c1bfa93a201bf7a07688af09c11
Kevin van Dijk <kevin@edgybits.com>
2017-04-21 17:52:58 +0200

    More tweaking

--- buddybuild_postclone.sh
+++ buddybuild_postclone.sh
@@ -5,3 +5,7 @@ export GIT_REV="$(git rev-parse --short HEAD)"
 # For sentry to know where to post errors to
 REVISION_FILE="./config/revision.json"
 echo "{\"revision\": \"${GIT_REV}\"}" > $REVISION_FILE
+
+echo "POST CLONE"
+pwd
+cat ./config/revision.json
