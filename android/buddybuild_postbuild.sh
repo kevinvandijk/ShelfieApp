23cd6490437ffb1e175375b0959071455ec48a5d
Kevin van Dijk <kevin@edgybits.com>
2017-04-21 04:18:55 +0200

    Fix typo

--- android/buddybuild_postbuild.sh
+++ android/buddybuild_postbuild.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 APP_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-MANIFEST_PATH = "$APP_PATH/android/app/src/main/AndroidManifest.xml"
+MANIFEST_PATH="$APP_PATH/android/app/src/main/AndroidManifest.xml"
 
 export VERSION_NUMBER="$(grep versionName $MANIFEST_PATH | sed 's/[^0-9\.]*//g')"
 export BUILD_NUMBER="$(grep versionCode $MANIFEST_PATH | sed 's/[^0-9]*//g')"
