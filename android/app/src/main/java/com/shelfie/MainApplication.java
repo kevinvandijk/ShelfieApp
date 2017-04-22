349812bf1e5ffdff4efd5db744f33a0916039ea1
Kevin van Dijk <kevin@edgybits.com>
2017-04-22 03:38:35 +0200

    Fix Chromecast support for iOS and Android (#10)

--- android/app/src/main/java/com/shelfie/MainApplication.java
+++ android/app/src/main/java/com/shelfie/MainApplication.java
@@ -3,6 +3,7 @@ package com.shelfie;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.googlecast.GoogleCastPackage;
 import com.oblador.keychain.KeychainPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.react.rnspinkit.RNSpinkitPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new GoogleCastPackage(),
             new KeychainPackage(),
             new VectorIconsPackage(),
             new RNSpinkitPackage(),
