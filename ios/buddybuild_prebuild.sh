00490be84ae787f8f8fc8a64d1b9a6b7e119f83d
Kevin van Dijk <kevin@edgybits.com>
2017-12-04 22:27:34 +0100

    Disable all buddybuild hooks

--- ios/buddybuild_prebuild.sh
+++ ios/buddybuild_prebuild.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ "$BUDDYBUILD_BRANCH" = "master" ]]; then
-  APP_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
-  $APP_PATH/scripts/setup-react-native-bundler.sh
-fi
+# if [[ "$BUDDYBUILD_BRANCH" = "master" ]]; then
+#   APP_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+#   $APP_PATH/scripts/setup-react-native-bundler.sh
+# fi
